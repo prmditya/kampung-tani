9cede9a6a605f8bfca67f0162a4a8540ce148453
Thoriq Kusuma <thoriqk2@gmail.com>
2025-10-27 14:24:04 +0700

    style: edit globals.css color only

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
-import { Topbar } from "@/components/topbar";
-import { cookies } from "next/headers";
+import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+import { Topbar } from '@/components/topbar';
+import { cookies } from 'next/headers';
 
 export default async function DashboardLayout({
   children,
@@ -9,13 +9,13 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   const cookieStore = await cookies();
-  const defaultOpen = cookieStore.get("sidebar:state")?.value !== "false";
+  const defaultOpen = cookieStore.get('sidebar:state')?.value !== 'false';
 
   return (
     <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar />
       <SidebarInset>
-        <div className="flex min-h-screen w-full flex-col overflow-x-hidden rounded-xl shadow-lg">
+        <div className="flex min-h-screen w-full flex-col overflow-x-hidden rounded-xl shadow-lg border-1">
           <Topbar />
           <main className="flex-1 bg-muted/40 p-3 sm:p-4 md:p-6 lg:p-8 overflow-x-hidden">
             <div className="max-w-[100vw] sm:max-w-none">{children}</div>
